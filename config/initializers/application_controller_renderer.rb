2959be336e98c00eaf69880ebccb3dfb643432bc
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-22 17:28:20 +0900

    [Add]Editing User

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4eb39623cd82988b849e886c7ab2a00c8340afa7a8375b9aa154c7dd8d68064da81d89d9996489ce74b51ac78ed1196fbe371d5ba16320072c4a98c497c5cccc'
