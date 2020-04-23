024d4e1881ad0063f84308120402deacd414e16d
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-23 10:09:25 +0900

    [Add]root_path

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
-  root 'books#index'
   resources :users, only: [:show,:index, :edit, :update]
   get 'home/about' => 'home#about'
-  root to: 'home#top'
+  get '' => 'home#top', as:'home_top'
 end
