65c37314fc33978c314c39d36a61878bf5362d60
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2020-04-23 14:10:29 +0900

    [Fix]LogIn Function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,10 @@ class ApplicationController < ActionController::Base
   end
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    added_attrs = [ :email, :name, :password, :password_confirmation ]
+    ex = [ :name]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: ex
   end
 end
